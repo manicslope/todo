059e7dadf340257bd44d5b82c3454681da08ad16
slope <slope@none.com>
2026-01-03 17:01:50 +0100

    pylint

--- tests.py
+++ tests.py
@@ -4,4 +4,4 @@ from main import parse_recurrence
 def test_parse_recurrence():
     assert parse_recurrence('1y') == 365
     assert parse_recurrence('2y') == 2 * 365
-    assert parse_recurrence('0') == None
+    assert parse_recurrence('0') is None
